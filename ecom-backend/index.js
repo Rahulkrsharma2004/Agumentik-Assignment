2796c5192608b135ab0ee98c0b0839c2e976732d
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2025-03-18 21:54:34 +0530

    cors policy config

--- ecom-backend/index.js
+++ ecom-backend/index.js
@@ -11,7 +11,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({origin: ["http://localhost:5173","https://ecommerce-backend-phi-green.vercel.app","https://agumentik-ecom.vercel.app"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
